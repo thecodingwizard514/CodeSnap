1dfb7b8e8877483ab918dbe46ad108bc0c194b56
thecodingwizard514 <taylorpitts964@gmail.com>
2024-09-27 11:32:48 +0530

    fix: updated build script for vercel feature: added a link in the Home Page for signup page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Link } from "@nextui-org/link";
+
 export default function Home() {
-    return <div>Home Page</div>;
+    return (
+        <div>
+            Home Page
+            <Link href="/sign-up">Sign up</Link>
+        </div>
+    );
 }
